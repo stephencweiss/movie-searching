ed57d7da5cd8e5e8db4f798b656dd816ea1f4c40
Stephen <stephen.c.weiss@gmail.com>
2020-03-14 16:29:21 -0500

    feat: add debounce

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import _ from "lodash";
 import { searchData } from "./helpers/searchData";
 
-const POSTER_PLACEHOLDER = 'https://www.theprintworks.com/wp-content/themes/psBella/assets/img/film-poster-placeholder.png'
+const POSTER_PLACEHOLDER =
+  "https://www.theprintworks.com/wp-content/themes/psBella/assets/img/film-poster-placeholder.png";
 
 const searchInput = document.querySelector(".search-input");
 const searchResults = document.querySelector(".results");
@@ -9,7 +11,6 @@ function showSearchResults(searchQuery) {
   const regex = new RegExp(searchQuery, "gi");
   searchData(searchQuery).then(results => {
     const html = results.map(movie => {
-      console.log({ movie });
       const title = movie.Title.replace(
         regex,
         `<span class='query-highlight'>${searchQuery}</span>`
@@ -21,7 +22,9 @@ function showSearchResults(searchQuery) {
           <span class='title'>${title}</span>
           <span class='year'>${movie.Year}</span>
         </div>
-        <img class='cropped-poster' src=${movie.Poster !== 'N/A' ? movie.Poster : POSTER_PLACEHOLDER}></img>
+        <img class='cropped-poster' src=${
+          movie.Poster !== "N/A" ? movie.Poster : POSTER_PLACEHOLDER
+        }></img>
         </li>
     `;
     });
@@ -34,5 +37,7 @@ function handleChange() {
   showSearchResults(this.value);
 }
 
-searchInput.addEventListener("change", handleChange);
-searchInput.addEventListener("keyup", handleChange);
+const optimizedHandleChange = _.debounce(handleChange,200);
+
+searchInput.addEventListener("change", optimizedHandleChange);
+searchInput.addEventListener("keyup", optimizedHandleChange);
