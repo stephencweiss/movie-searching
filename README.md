f5f8a0cc1d477476822e045307f76701c2e3a209
Stephen <stephen.c.weiss@gmail.com>
2020-03-14 16:19:10 -0500

    feat: styling of the results

---
+++ README.md
@@ -0,0 +1,7 @@
+A simple example of hitting the OMDd API with some search optimizations
+
+## Getting Started
+After an initial installation of dependencies, you can launch the app with:
+```shell
+$ yarn start
+```
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { searchData } from "./helpers/searchData";
 
+const POSTER_PLACEHOLDER = 'https://www.theprintworks.com/wp-content/themes/psBella/assets/img/film-poster-placeholder.png'
+
 const searchInput = document.querySelector(".search-input");
 const searchResults = document.querySelector(".results");
 
@@ -14,10 +16,12 @@ function showSearchResults(searchQuery) {
       );
 
       return `
-      <li >
-        <span class='title'>${title}</span>
-        <span class='year'>${movie.Year}</span>
-        <img src=${movie.Poster}></img>
+      <li class='movie-result'>
+        <div class='movie-details'>
+          <span class='title'>${title}</span>
+          <span class='year'>${movie.Year}</span>
+        </div>
+        <img class='cropped-poster' src=${movie.Poster !== 'N/A' ? movie.Poster : POSTER_PLACEHOLDER}></img>
         </li>
     `;
     });
